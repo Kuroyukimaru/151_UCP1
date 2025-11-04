0b1475a576de2856751533509969103270e22a15
“Kuroyukimaru” <oranggabut712@gmail.com>
2025-11-04 13:52:22 +0700

    buat file kebunbinatang.js dan isinya di dalam folder models

---
+++ models/kebunbinatang.js
@@ -0,0 +1,36 @@
+odule.exports = (sequelize, DataTypes) => {
+  const kandang = sequelize.define('kandang', {
+    ID: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    Nama_hewan: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    Nama_petugas: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    Usia_hewan: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    Jenis_hewan: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    Tahun_lahir: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+
+  }, {
+    tableName: 'kandang',
+    timestamps: false,
+    freezeTableName: true
+  });
+
+  return film;
+};
