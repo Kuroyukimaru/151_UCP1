0f720f867a44326a841090ffa2930d8b1fc9e526
“Kuroyukimaru” <oranggabut712@gmail.com>
2025-11-04 13:56:41 +0700

    coding di index.js

---
+++ index.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const app = express();
+const db = require('./models');
+const PORT = process.env.PORT || 3000;
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// === CRUD ===
+
+// CREATE
+app.post('/kandang', async (req, res) => {
+  try {
+    const kandang = await db.kandang.create(req.body);
+    res.send(kandang);
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+});
+
+// READ
+app.get('/kandang', async (req, res) => {
+  try {
+    const film = await db.kandang.findAll();
+    res.send(kandang);
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+});
+
+// UPDATE
+app.put('/kandang/:id', async (req, res) => {
+  const id = req.params.id;
+  try {
+    const kandang = await db.kandang.findByPk(id);
+    if (!kandang) {
+      return res.status(404).send({ message: "Kandang not found" });
+    }
+    await kandang.update(req.body);
+    res.send({ message: "Kandang berhasil diupdate", data: kandang });
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+});
+
+// DELETE
+app.delete('/kandang/:id', async (req, res) => {
+  const id = req.params.id;
+  try {
+    const kandang = await db.kandang.findByPk(id);
+    if (!kandang) {
+      return res.status(404).send({ message: "Kandang not found" });
+    }
+    await kandang.destroy();
+    res.send({ message: "Kandang berhasil dihapus" });
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+});
+
+db.sequelize.sync()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error('Database sync error:', err);
+  });
